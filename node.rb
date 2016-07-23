9233ea25ac6191e359f6f62b73fc52ca6e8bc521
Danny Bravman <dbravman@gmail.com>
2016-07-23 12:53:30 -0500

    node

--- node.rb
+++ node.rb
@@ -1,3 +1,20 @@
 class Node
+  
 
+  def initialize(value)
+    @value = value
+    @children = []
+  end
+
+  def add_child(child)
+    @children << child
+  end
+
+  def value
+    @value
+  end
+
+  def children
+    
+  end
 end
