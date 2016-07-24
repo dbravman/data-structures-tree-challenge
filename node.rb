553e69c1607e5d8e88d87de2dc622ae1317df905
Danny Bravman <dbravman@gmail.com>
2016-07-23 21:23:43 -0500

    search finds test

--- node.rb
+++ node.rb
@@ -9,4 +9,16 @@ class Node
   def add_child(child)
     @children << child
   end
+
+  def search(&block)
+    return self if block.call == @value
+
+    @children.each do |node|
+      result = node.search(&block)
+      if result != nil
+        return result
+      end
+    end
+    nil 
+  end
 end
